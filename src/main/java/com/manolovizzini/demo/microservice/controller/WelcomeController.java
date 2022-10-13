19af412f99244d586993fb6e8a8d2aaf8bcbdd8d
ManoloComo78 <Gabber1978>
2022-10-13 15:15:00 +0200

    First import

--- src/main/java/com/manolovizzini/demo/microservice/controller/WelcomeController.java
+++ src/main/java/com/manolovizzini/demo/microservice/controller/WelcomeController.java
@@ -1,16 +1,35 @@
 package com.manolovizzini.demo.microservice.controller;
 
+import com.manolovizzini.demo.microservice.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author mviz - 13/10/2022
  * @version 1.0-SNAPSHOT
  * <p>
  */
-@Controller
+@RestController
 public class WelcomeController {
+
+    @Autowired
+    private final UserService userService;
+
+    public WelcomeController(UserService userService) {
+        this.userService = userService;
+    }
+
+//    @GetMapping("/")
+//    public String viewHomePage(Model model) {
+//        //model.addAttribute("users", userService.findAll());
+//        return "welcome";
+//    }
+
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcome() {
         return "welcome";
