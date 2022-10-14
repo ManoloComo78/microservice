5b57bb3f534bdeb5e2ca329d9e8936b30de1ba23
ManoloComo78 <Gabber1978>
2022-10-14 10:54:30 +0200

    First import

---
+++ src/main/java/com/manolovizzini/demo/microservice/ApplicationMicroservice.java
@@ -0,0 +1,50 @@
+package com.manolovizzini.demo.microservice;
+
+import com.manolovizzini.demo.microservice.dao.user.UserRepository;
+import com.manolovizzini.demo.microservice.domain.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author mviz - 13/10/2022
+ * @version 1.0-SNAPSHOT
+ * <p>
+ */
+@SpringBootApplication
+@EntityScan("com.manolovizzini.demo.microservice.domain")
+@EnableJpaRepositories("com.manolovizzini.demo.microservice.dao")
+public class ApplicationMicroservice extends SpringBootServletInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationMicroservice.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMicroservice.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserRepository repository) {
+        return args -> {
+            repository.save(new User("Java"));
+            repository.save(new User("Node"));
+            repository.save(new User("Python"));
+
+            System.out.println("\nfindAll()");
+            repository.findAll().forEach(x -> System.out.println(x.getUsername()));
+
+            System.out.println("\nfindById(1L)");
+            repository.findById(1l).ifPresent(x -> System.out.println(x.getUsername()));
+
+//        System.out.println("\nfindByName('Node')");
+//        repository.findByUsername("Node").forEach(x -> System.out.println(x));
+        };
+    }
+
+
+}
